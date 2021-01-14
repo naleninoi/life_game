aa84f04f58706c03a581097c8c3acf27c48fbfa1
naleninoi <naleninoi@gmail.com>
2021-01-14 16:57:37 +0300

    minor html improvements

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
 
+export class FooterComponent implements OnInit {
+  myDate = new Date();
   constructor() { }
 
   ngOnInit() {
--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-gameboard',
   templateUrl: './gameboard.component.html',
   styleUrls: ['./gameboard.component.css']
-})
+ })
 export class GameboardComponent implements OnInit {
 
   @Input() cells;
   @Input() generation;
+  myDate = new Date();
 
   constructor() { }
 
